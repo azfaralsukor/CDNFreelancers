c0933d981939e17fb3184c570d3e3f6f22585ed5
gigantor <azfaralsukor@gmail.com>
2020-01-14 15:02:28 +0800

    Updates

---
+++ precache-manifest.35b46c531c678ada7a8bc5c4b1f3eb0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907fdbf34d4cc858b70d22bcb929e6f8",
+    "url": "/minimalUserManagement/index.html"
+  },
+  {
+    "revision": "d3ba77b9d91bd472e787",
+    "url": "/minimalUserManagement/static/css/main.da005ed0.chunk.css"
+  },
+  {
+    "revision": "a79c0a6a8b0cfbca7b4a",
+    "url": "/minimalUserManagement/static/js/2.f97b1c18.chunk.js"
+  },
+  {
+    "revision": "d3ba77b9d91bd472e787",
+    "url": "/minimalUserManagement/static/js/main.918dcc0b.chunk.js"
+  },
+  {
+    "revision": "dad2eee03089e926b61c",
+    "url": "/minimalUserManagement/static/js/runtime~main.1c5f00f4.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/minimalUserManagement/precache-manifest.35b46c531c678ada7a8bc5c4b1f3eb0e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/minimalUserManagement/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
